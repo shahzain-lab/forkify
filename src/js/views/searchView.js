0dd385eb4904c46bdc552445f5613e51e6cdcd57
shahzain-lab <shahzain30196@gmail.com>
2008-02-18 23:03:12 -0800

    pagination added

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,18 +3,18 @@ import { elements } from "./base"
 export const getInputVal = () => elements.searchInput.value;
 
 export const clearFeild = () => {
-    elements.searchInput.value = "";
+  elements.searchInput.value = "";
 }
 
 export const clearResults = () => {
-    elements.racipeResList.innerHTML = '';
+  elements.racipeResList.innerHTML = '';
 }
 
 const limitRacipeTitle = (title, limit = 17) => {
   const newTitle = [];
-  if(title.length > limit){
-    title.split(" ").reduce((acc ,cur) => {
-      if(acc + cur.length <= limit){
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
         newTitle.push(cur);
       }
       return acc + cur.length;
@@ -25,7 +25,7 @@ const limitRacipeTitle = (title, limit = 17) => {
 }
 
 const renderRacipe = racipe => {
-    const markup = `
+  const markup = `
     <li class="preview">
     <a class="preview__link preview__link--active" href="#${racipe.recipe_id}">
       <figure class="preview__fig">
@@ -43,9 +43,46 @@ const renderRacipe = racipe => {
     </a>
   </li>
     `
-    elements.racipeResList.insertAdjacentHTML('beforeend', markup);
+  elements.racipeResList.insertAdjacentHTML('beforeend', markup);
 }
 
-export const renderResults = racipes => {
-    racipes.forEach(renderRacipe)
+const createButton = (page, type) => `
+     <button class="btn--inline pagination__btn--${type}">
+        <svg class="search__icon">
+           <use href="img/icons.svg#icon-arrow-${type === 'prev' ? 'left' : 'right'}"></use>
+        </svg>
+        <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+    </button>
+
+    <!-- <button class="btn--inline pagination__btn--prev">
+    <svg class="search__icon">
+      <use href="img/icons.svg#icon-arrow-left"></use>
+    </svg>
+    <span>Page 1</span>
+    </button>
+    <button class="btn--inline pagination__btn--next">
+    <span>Page 3</span>
+    <svg class="search__icon">
+      <use href="img/icons.svg#icon-arrow-right"></use>
+    </svg>
+    </button> -->
+`
+
+const renderButton = (page, numResults, resPerPage) => {
+  const pages = Math.ceil(numResults / resPerPage);
+
+  if (page === 1 && pages > 1) {
+    //only show the next btn
+  } else if (page < pages) {
+    //both btns
+  } else if (page === pages && pages > 1) {
+    //only show the prev btn
+  }
+}
+
+export const renderResults = (racipes, page = 1, resPerPage = 10) => {
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
+
+  racipes.slice(start, end).forEach(renderRacipe);
 }
