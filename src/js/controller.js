3348f4765d7299c6c58c45d2b48d921a7add84bd
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-08-28 07:34:11 +0500

    initialized NPM and parcel

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,4 +10,30 @@ const timeout = function (s) {
 
 // https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+// 1a95e858-c21c-4674-9549-a53349dd0a4b
+/////////////////////////////////////
+
+(async function() {
+  try{
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const data = await res.json();
+    if(!res.ok) throw new Error(`${res.status}, ${data.message}`)
+    
+    let {recipe} = data.data;
+    
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    }
+    console.log(recipe);
+
+  }catch(err) {
+    console.log(err);
+  }
+})();
