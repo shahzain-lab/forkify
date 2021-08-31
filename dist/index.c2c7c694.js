a70724cd15d044fe649e918ee942e4b2deae834b
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-08-31 09:12:59 +0500

    BUGFIX

--- dist/index.c2c7c694.js
+++
@@ -1,2 +0,0 @@
-const recipeContainer=document.querySelector(".recipe"),timeout=function(e){return new Promise((function(o,t){setTimeout((function(){t(new Error(`Request took too long! Timeout after ${e} second`))}),1e3*e)}))};
-//# sourceMappingURL=index.c2c7c694.js.map
